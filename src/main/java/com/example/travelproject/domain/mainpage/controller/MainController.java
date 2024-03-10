86ee34a21f8f4d6baf361c74d6eec7d45505ac40
= <=>
2024-03-10 14:27:18 +0900

    Upload Initial Project

---
+++ src/main/java/com/example/travelproject/domain/mainpage/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.example.travelproject.domain.mainpage.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@RequestMapping("/")
+public class MainController {
+
+}
