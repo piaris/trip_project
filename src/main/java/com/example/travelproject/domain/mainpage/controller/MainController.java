a60838410c4e9ab9faf2d55c0eb72b56eab70041
dianacho <piaristar@gmail.com>
2024-03-11 16:08:59 +0900

    user branch commit

--- src/main/java/com/example/travelproject/domain/mainpage/controller/MainController.java
+++ src/main/java/com/example/travelproject/domain/mainpage/controller/MainController.java
@@ -6,9 +6,11 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.view.RedirectView;
 
+import com.example.travelproject.domain.user.model.UserEntity;
 import com.example.travelproject.domain.user.service.UserService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -19,27 +21,50 @@ public class MainController {
     @Autowired
     private UserService userService;
 
+    // http://localhost:8080/
     @GetMapping("/")
     public RedirectView redirectToMain() {
         return new RedirectView("/main");
     }
 
-    @GetMapping("/loginPage")
-    public String loginPage(@RequestParam(value = "errorMessage", required = false) String errorMessage, Model model) {
-
-        model.addAttribute("errorMessage", errorMessage);
-        return "login/loginPage";
-    }
-
+    // http://localhost:8080/main
     @GetMapping("/main")
-    public String mainIndex(Authentication authentication, Model model){
-        if(authentication != null) {
+    public String mainIndex(Authentication authentication, Model model) {
+        if (authentication != null) {
             UserDetails userDetails = (UserDetails) authentication.getPrincipal();
             model.addAttribute("username", userDetails.getUsername());
-            
+
         }
-        model.addAttribute("menuTitle","홈");
+        model.addAttribute("menuTitle", "홈");
         return "index";
     }
+
+    // join 화면: http://localhost:8080/joinPage
+    @GetMapping("/joinPage")
+    public String joinPage() {
+        return "login/join";
+    }
+
+    // join 요청 -> login 화면으로 이동
+    @PostMapping("/join")
+    public String join(@ModelAttribute UserEntity dto) {
+        log.info("[MainController][join] dto >" + dto.toString());
+        userService.joinUserDto(dto);
+        return "redirect:/login/login";
+    }
+
+    // login 화면: http://localhost:8080/loginPage
+    @GetMapping("/loginPage")
+    public String loginPage() {
+        return "login/login";
+    }
+
+    // login 요청 -> 성공화면, 가입자 화면
+    // @PostMapping("/login")
+    // public String login(@ModelAttribute UserEntity dto, HttpServletRequest
+    // request) {
+    // UserEntity user = userService.loginUser(dto);
+
+    // }
 
 }
